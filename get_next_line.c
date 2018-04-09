4704fdc4bf28486ce4aca1d7bf3d163d9d0c3c9e
Oleksiiy PAVLIUK <opavliuk@e3r2p9.unit.ua>
2018-04-09 21:53:51 +0300

    changed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: opavliuk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 12:57:52 by opavliuk          #+#    #+#             */
-/*   Updated: 2018/04/09 21:27:39 by opavliuk         ###   ########.fr       */
+/*   Updated: 2018/04/09 21:52:59 by opavliuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,22 @@
 void	check_NL(t_str *gnl, char *buffer, char **line)
 {
 	int i;
+	int n;
 
 	i = 0;
-	while (buffer[i])
+	n = 0;
+	while (gnl->buffer[i])
 	{
-		if (buffer[i] == '\n')
+		if (gnl->buffer[i] == '\n')
 			break ;
 		i++;
 	}
-	(*line) = ft_strsub(buffer, 0, i);
+	(*line) = ft_strsub(gnl->buffer, 0, i);
 	printf("%d\n", i);
-	if (gnl->n == 1)
-	{
-		gnl->string = ft_strjoin(gnl->mod, buffer + (i + 1));
-		ft_strdel(&(gnl->mod));
-	}
-	else
-		gnl->string = ft_strsub(buffer, i + 1, ft_strlen(buffer + (i + 1)));
-	gnl->mod = ft_strdup(gnl->string);
-	ft_strdel(&(gnl->string));
+	i++;
+	n = ft_strlen(gnl->buffer + (i + 1));
+	ft_memmove(&(gnl->buffer[0]), &(gnl->buffer[i]), n);
+	ft_memset(gnl->buffer, '\0', ft_strlen(gnl->buffer + n));
 }
 
 int		get_next_line(const int fd, char **line)
@@ -49,8 +46,8 @@ int		get_next_line(const int fd, char **line)
 	gnl = malloc(sizeof(t_str));
 	if (fd)
 	{
-		k = read(fd, buffer, BUFF_SIZE);
-		check_NL(gnl, buffer, line);
+		k = read(fd, gnl->buffer, BUFF_SIZE);
+		check_NL(gnl, gnl->buffer, line);
 		if (!k)
 			return (0);	
 		return (1);
